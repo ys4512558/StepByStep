d0acf6c74fe1966e812a7540023a119ebadd80b3
Beudicri <ys4512558@naver.com>
2023-05-07 16:01:03 +0900

    kakaoLogin 수정

--- chageunchageun/src/main/java/com/chageunchageun/chageunchageun/service/KakaoService.java
+++ chageunchageun/src/main/java/com/chageunchageun/chageunchageun/service/KakaoService.java
@@ -1,9 +1,12 @@
 package com.chageunchageun.chageunchageun.service;
 
+import com.chageunchageun.chageunchageun.controller.MemoirController;
 import com.chageunchageun.chageunchageun.data.dto.KakaoDTO;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -16,12 +19,15 @@ import java.util.HashMap;
 @Service
 public class KakaoService {
 
+    private final Logger log = LoggerFactory.getLogger(KakaoService.class);
     //코드를 통해 토큰 발급
     public String getAccessToken(String code){
+
         String access_Token = "";
         String refresh_Token = "";
         String reqURL = "https://kauth.kakao.com/oauth/token";
 
+        System.out.println("Code : " + code);
         URL url = null;
         try {
             url = new URL(reqURL);
@@ -35,13 +41,13 @@ public class KakaoService {
 
             sb.append("grant_type=authorization_code");
             sb.append("&client_id=247878f49f226931118bb5976aad05a8");
-            sb.append("&redirect_uri=http://localhost:8080/chageunchageun/auth/KakaoLogin");
+            sb.append("&redirect_uri=http://localhost:8080/api/v1/auth/KakaoLogin");
             sb.append("&code=" + code);
             bw.write(sb.toString());
             bw.flush();
 
             int responseCode = conn.getResponseCode();
-            //System.out.println("responseCode : " + responseCode);
+            System.out.println("responseCode : " + responseCode);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
@@ -51,7 +57,7 @@ public class KakaoService {
                 result += line;
             }
 
-            //System.out.println("response body : " + result);
+            System.out.println("response body : " + result);
 
             JsonParser jsonParser = new JsonParser();
             JsonElement element = jsonParser.parse(result);
@@ -59,8 +65,8 @@ public class KakaoService {
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
             refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            //System.out.println("access_token : " + access_Token);
-            //System.out.println("refresh_token : " + refresh_Token);
+            System.out.println("access_token : " + access_Token);
+            System.out.println("refresh_token : " + refresh_Token);
 
             br.close();
             bw.close();
@@ -99,16 +105,20 @@ public class KakaoService {
             }
             System.out.println("response body : " + result);
 
+            //이부분 오류
+            int i = 0;
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
 
             JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
             JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 
-            String name = properties.getAsJsonObject().get("name").getAsString();
-            String image = properties.getAsJsonObject().get("image").getAsString();
+            log.info("\n");
+            log.info("log = {}", properties);
+            log.info("log = {}", kakao_account);
+            String name = properties.getAsJsonObject().get("nickname").getAsString();
+            String image = properties.getAsJsonObject().get("profile_image").getAsString();
             String email = kakao_account.getAsJsonObject().get("email").getAsString();
-
             userInfo.put("name", name);
             userInfo.put("email", email);
             userInfo.put("image", image);
